99ea9570d5f908c0ec2130bc35f32ff8bcf27e94
Hao Zhang <hao@hao-zhang.com>
2017-07-04 22:49:29 -0600

    Small changes to make SWIG work

--- ql/utilities/stringutils.hpp
+++ ql/utilities/stringutils.hpp
@@ -11,6 +11,13 @@ namespace {
         std::for_each(input.cbegin(), input.cend(), [&ret](const char c) { ret.push_back(std::toupper(c)); });
         return ret;
     }
+
+    inline std::string to_lower_copy(const std::string &input) {
+        std::string ret;
+        std::for_each(input.cbegin(), input.cend(), [&ret](const char c) { ret.push_back(std::tolower(c)); });
+        return ret;
+    }
+
 }
 
 #endif
